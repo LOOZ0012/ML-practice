c37a5470216cc01451d4c2326299085b4a3b67ef
LOOZ0012 <loozhonghan@outlook.com>
2023-11-25 20:53:52 +0800

    Added a lot of stuff

--- project2.py
+++ project2.py
@@ -12,6 +12,20 @@ from random import shuffle
 import pandas as pd
 import re
 
+####### Zhong Han's note:
+####### Added a Lemmatizer so that we can lemmatize our SMS and email datasets
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
+
+####### Zhong Han's note:
+####### Type the following line into your VScode terminal 
+####### $pip install -U scikit-learn
+
+import sklearn
+from sklearn.svm import SVC
+from nltk.classify.scikitlearn import SklearnClassifier
+
+
 ## The corpora you will primarily be using are located in the `datasets`
 ## subfolder, and a brief explanation of each are given here:
 ## `SMSSpamCollection.txt` - this is the full UCI dataset, which is compiled
@@ -124,6 +138,17 @@ spam_words = [word.lower() for word, tag in filter(
 
 print("number of `spam` words:", len(spam_words))
 
+
+####### Zhong Han's note:
+####### I added a lemmatizer at this step!
+
+# Initializing a Lemmatizer to lemmatize the words
+lemmatizer = WordNetLemmatizer()
+
+# Lemmatizing the words after tokenization for ham messages and spam messages
+ham_words = [lemmatizer.lemmatize(word.lower()) for sentence in ham_tags for word, tag in sentence]
+spam_words = [lemmatizer.lemmatize(word.lower()) for sentence in spam_tags for word, tag in sentence]
+
 print("Getting the frequency distributions...")
 # now let's create frequency distributions of each list of words
 ham_fd = nltk.FreqDist(ham_words)
@@ -138,39 +163,48 @@ for word in common_set:
     del ham_fd[word]
     del spam_fd[word]
 
-# create a dictionary of the top 100 words remaining in each frequency distribution
-top_100_ham = {word for word, count in ham_fd.most_common(100)}
-top_100_spam = {word for word, count in spam_fd.most_common(100)}
+####### Zhong Han's note:
+####### I changed from "Top 100" to "Top 30". This resulted in an immediate boost of accuracy from 83% to 88%.
+####### I suspect it's because the original "Top 100" most common words wasn't specific enough.
 
-# We now have a set of features consisting of the top 100 words occurring uniquely
+# create a dictionary of the top 30 words remaining in each frequency distribution
+top_30_ham = {word for word, count in ham_fd.most_common(30)}
+top_30_spam = {word for word, count in spam_fd.most_common(30)}
+
+# We now have a set of features consisting of the top 30 words occurring uniquely
 # in `ham` and `spam` messages.
-print(top_100_ham)
-print(top_100_spam)
+print(top_30_ham)
+print(top_30_spam)
 
-# Define a function to count exclamation points
-def count_exclamation(text):
-    return text.count('!')
-
-# Update the extract_features function to include bigrams and count exclamation points
+# We can now define a function to count these 'features' (words) in each sentence
 def extract_features(text):
     features = dict()
     word_count_ham = 0
     exclamation_count = 0  # initialize the count of exclamation points
 
-    # Tokenize text into bigrams
-    bigrams = list(nltk.bigrams(nltk.word_tokenize(text)))
+    ####### Zhong Han's note:
+    ####### I tried to expand the scope a bit further by using bigrams instead of single words.
+    ####### This part of the code is basically combining bigrams with lemmatization.
 
-    for bigram in bigrams:
-        # Check if both words in the bigram are in the top_100_ham list
-        if bigram[0].lower() in top_100_ham and bigram[1].lower() in top_100_ham:
+    # Tokenize text into lemmatized bigrams
+    lemmatized_bigrams = list(nltk.bigrams([lemmatizer.lemmatize(word.lower()) for word in nltk.word_tokenize(text)]))
+
+    for bigram in lemmatized_bigrams:
+        # Check if both words in the lemmatized bigram are in the top_30_ham list
+        if bigram[0] in top_30_ham and bigram[1] in top_30_ham:
             word_count_ham += 1
 
+    ####### Zhong Han's note:
+    ####### Originally, the starter code only had the "word count" feature (i.e. the classifier uses "most common spam/ham words" as a feature)
+    ####### I added a second feature, counting how many "exclamation points" were in the dataset. This boosted accuracy by 1%.
+    
     # Count exclamation points using regular expression
     exclamation_count = len(re.findall(r'!', text))
 
     features["wordcount_ham"] = word_count_ham
     features["exclamation_count"] = exclamation_count  # include the count of exclamation points as a feature
     return features
+
 
 print("Extracting features...")
 # Now we can create a new list consisting of a dictionary of features for each
@@ -195,20 +229,40 @@ print(len(features))
 # Now that we have 'featurized' all our text, we can use these features to train
 # our classifier.
 
+####### Zhong Han's note:
+####### So this is the part where we start using classifiers
+####### Originally, the starter code used only NLTK's Naive Bayes (NB) Classifer
+####### Anytime you see the variable "classifier", it refers to the original NLTK NB Classifier
+####### However, I'm going to utilize an SVM Classifier from scikit-learn (sklearn), a library meant for machine learning (whereas NLTK is meant for NLP)
+####### Find out more at this link: https://scikit-learn.org/stable/ 
+
 # First we get the length of a quarter of the dataset
 train_count = len(features) // 4
 
 shuffle(features) # shuffle the dataset
 
-print(F"Training classifier on {train_count} samples")
+####### Zhong Han's note:
+####### NLTK's NB classifier:
+print(F"Training NLTK's NB classifier on {train_count} samples")
 # instantiate a classifier that trains on the first quarter of the dataset
 classifier = nltk.NaiveBayesClassifier.train(features[:train_count])
 # print out the 10 features that most clearly distinguish between positive/negative classes
 classifier.show_most_informative_features(10)
 
-print(F"Assessing classifier on {len(features)-train_count} samples")
+####### Zhong Han's note:
+####### sklearn's SVM classifier:
+print(F"Training SVM classifier on {train_count} samples")
+svm_classifier = SklearnClassifier(SVC())
+svm_classifier.train(features[:train_count])
+
+print(F"Assessing NLTK's NB classifier on {len(features)-train_count} samples")
 # assess how accurate the trained classifier is on the last three quarters of the dataset
-print("Accuracy of classifier:", nltk.classify.accuracy(classifier, features[train_count:]))
+print("Accuracy of NLTK's NB classifier:", nltk.classify.accuracy(classifier, features[train_count:]))
+
+print(F"Assessing SVM classifier on {len(features)-train_count} samples")
+# assess how accurate the trained classifier is on the last three quarters of the dataset
+accuracy_svm = nltk.classify.accuracy(svm_classifier, features[train_count:])
+print("Accuracy of SVM classifier:", accuracy_svm)
 
 # Now let's import an unseen dataset of different data that has also been
 # labeled as spam/ham. This is email data from the Linguist List, which I have
@@ -227,11 +281,11 @@ efeatures = [
     # for text in tqdm(ham_sms[1]) # uncomment this line and comment out the line above to view progress
     ]
 # Then we assess our classifier's accuract on the featurized texts
-print(F"Assessing classifier on {len(efeatures)} samples")
-print("Accuracy of classifier on email data:", nltk.classify.accuracy(classifier, efeatures[:]))
+print(F"Assessing NLTK's NB classifier on {len(efeatures)} samples")
+print("Accuracy of NLTK's NB classifier on email data:", nltk.classify.accuracy(classifier, efeatures[:]))
 
-# Let's now try to add another feature to our classifier, to see whether we can
-# improve our score.
+print(F"Assessing SVM classifier on {len(efeatures)} samples")
+print("Accuracy of SVM classifier on email data:", nltk.classify.accuracy(svm_classifier, efeatures[:]))
 
 # You may notice that every time you run this code, your final accuracy score
 # will differ. This is because you are shuffling (randomizing) your set of
@@ -248,18 +302,34 @@ print("Accuracy of classifier on email data:", nltk.classify.accuracy(classifier
 # The code below runs 10 iterations of the classifier and writes each score to
 # a text file, along with a final average score (mean) across all 10 iterations.
 with open('output.txt', 'w') as f:
-    iterations = []
+    nb_iterations = []
+    svm_iterations = []
+
     for x in list(range(10)):
-        print(x)
-        shuffle(features) # shuffle the dataset
-        classifier = nltk.NaiveBayesClassifier.train(features[:]) # train a new classifier on all the sms data
-        accuracy = nltk.classify.accuracy(classifier, efeatures[:]) # check its accuracy on all the email data
-        iterations.append(accuracy) # store the accuracy value for this run
-        f.write(str(accuracy)+"\n") # write the value to our output file
-    f.write("\n"+str(mean(iterations))) # write the mean value to our output file
+        print("Writing iteration number: ", x)
+        shuffle(features)  # shuffle the dataset
+
+        # Train a new Naive Bayes classifier on all the sms data
+        nb_classifier = nltk.NaiveBayesClassifier.train(features[:])
+
+        # Check accuracy of Naive Bayes classifier on all the email data
+        nb_accuracy = nltk.classify.accuracy(nb_classifier, efeatures[:])
+        nb_iterations.append(nb_accuracy)
+
+        # Train a new SVM classifier on all the sms data
+        svm_classifier = SklearnClassifier(SVC())
+        svm_classifier.train(features[:])
+
+        # Check accuracy of SVM classifier on all the email data
+        svm_accuracy = nltk.classify.accuracy(svm_classifier, efeatures[:])
+        svm_iterations.append(svm_accuracy)
+
+        f.write(f"Iteration {x + 1}: {nb_accuracy} (Naive Bayes Accuracy)\n")
+        f.write(f"Iteration {x + 1}: {svm_accuracy} (SVM Accuracy)\n\n")
+
+    f.write("\nMean accuracy of Naive Bayes Classifier: " + str(mean(nb_iterations)))
+    f.write("\nMean accuracy of SVM Classifier: " + str(mean(svm_iterations)))
 
 # Now that you understand the basics of how to build a classifier, continue to
 # experiment with developing features that may help your classifier to
 # distinguish better between spam and ham.
-
-## ZH
